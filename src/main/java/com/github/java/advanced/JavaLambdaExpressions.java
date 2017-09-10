d37dd5cf52a430b0a2db5b65b836adeda4d9371a
Dave Cotton <david.f.cotton@gmail.com>
2017-09-10 10:30:44 +1000

    Finished JavaLambdaExpressions

--- src/main/java/com/github/java/advanced/JavaLambdaExpressions.java
+++ src/main/java/com/github/java/advanced/JavaLambdaExpressions.java
@@ -1,49 +1,87 @@
-//package com.github.java.advanced;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//import java.util.StringTokenizer;
-//
-//public class JavaLambdaExpressions {
-//
-//  public static void main(String[] args) throws IOException {
-//    MyMath ob = new MyMath();
-//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//    int T = Integer.parseInt(br.readLine());
-//    PerformOperation op;
-//    boolean ret = false;
-//    String ans = null;
-//    while (T-- > 0) {
-//      String s = br.readLine().trim();
-//      StringTokenizer st = new StringTokenizer(s);
-//      int ch = Integer.parseInt(st.nextToken());
-//      int num = Integer.parseInt(st.nextToken());
-//      if (ch == 1) {
-//        op = ob.isOdd();
-//        ret = ob.checker(op, num);
-//        ans = (ret) ? "ODD" : "EVEN";
-//      } else if (ch == 2) {
-//        op = ob.isPrime();
-//        ret = ob.checker(op, num);
-//        ans = (ret) ? "PRIME" : "COMPOSITE";
-//      } else if (ch == 3) {
-//        op = ob.isPalindrome();
-//        ret = ob.checker(op, num);
-//        ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
-//
-//      }
-//      System.out.println(ans);
-//    }
-//  }
-//}
-//
-//interface PerformOperation {
-//  boolean check(int a);
-//}
-//
-//class MyMath {
-//  public static boolean checker(PerformOperation p, int num) {
-//    return p.check(num);
-//  }
-//}
+package com.github.java.advanced;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class JavaLambdaExpressions {
+
+  public static void main(String[] args) throws IOException {
+    MyMath myMath = new MyMath();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine());
+    PerformOperation operation;
+    boolean ret;
+    String ans = null;
+
+    while (T-- > 0) {
+      String s = br.readLine().trim();
+      StringTokenizer tokenizer = new StringTokenizer(s);
+      int type = Integer.parseInt(tokenizer.nextToken());
+      int number = Integer.parseInt(tokenizer.nextToken());
+      if (type == 1) {
+        operation = myMath.isOdd();
+        ret = MyMath.checker(operation, number);
+        ans = (ret) ? "ODD" : "EVEN";
+      } else if (type == 2) {
+        operation = myMath.isPrime();
+        ret = MyMath.checker(operation, number);
+        ans = (ret) ? "PRIME" : "COMPOSITE";
+      } else if (type == 3) {
+        operation = myMath.isPalindrome();
+        ret = MyMath.checker(operation, number);
+        ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
+      }
+
+      System.out.println(ans);
+    }
+  }
+}
+
+interface PerformOperation {
+  boolean check(int number);
+}
+
+class MyMath {
+  static boolean checker(PerformOperation operation, int number) {
+    return operation.check(number);
+  }
+
+  // WRITE YOUR CODE HERE
+  ///////////////////////
+  PerformOperation isOdd() {
+    return n -> (n % 2 != 0);
+  }
+
+  PerformOperation isPrime() {
+//    return n -> java.math.BigInteger.valueOf(n).isProbablePrime(10);
+
+    return n -> {
+      if (n <= 1) {
+        return false;
+      }
+      if (n == 2) {
+        return true;
+      }
+      if (n % 2 == 0) {
+        return false;
+      }
+
+      int m = Double.valueOf(Math.ceil(Math.sqrt(n))).intValue();
+      for (int i = 3; i < m; i += 2) {
+        if (n % i == 0) {
+          return false;
+        }
+      }
+
+      return true;
+    };
+  }
+
+  PerformOperation isPalindrome() {
+    return n -> Integer.toString(n).equals(new StringBuilder(Integer.toString(n)).reverse().toString());
+  }
+  //////////////////////
+  // END WRITE CODE HERE
+}
